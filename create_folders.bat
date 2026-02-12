36a1cdd59dc2c68704e2855b268871e71622f436
admissiontest11 <paulxyz52@gmail.com>
2026-02-12 20:58:48 +0600

    Add files via upload

---
+++ create_folders.bat
@@ -0,0 +1,17 @@
+@echo off
+cd /d "C:\Users\user\Desktop\admission-test-helpline"
+mkdir data\ssc\বাংলা_১ম_পত্র\আম-আঁটির_ভেঁপু
+mkdir data\ssc\বাংলা_১ম_পত্র\কপোতাক্ষ_নদ
+mkdir data\ssc\বাংলা_১ম_পত্র\মানুষ_মুহম্মদ_(স.)
+mkdir data\ssc\বাংলা_১ম_পত্র\বই_পড়া
+mkdir data\ssc\বাংলা_১ম_পত্র\সুভা
+mkdir data\ssc\বাংলা_১ম_পত্র\নিমন্ত্রণ
+mkdir data\ssc\বাংলা_১ম_পত্র\আবার_আসিব_ফিরে
+mkdir data\ssc\বাংলা_১ম_পত্র\পল্লিজননী
+mkdir data\ssc\বাংলা_১ম_পত্র\বাংলা_মায়ের_ভাষা
+mkdir data\ssc\বাংলা_১ম_পত্র\সাহিত্যের_খেলা
+mkdir data\ssc\বাংলা_১ম_পত্র\মহাজাগতিক_খোঁজে
+mkdir data\ssc\বাংলা_১ম_পত্র\একাত্তরের_দিনগুলি
+mkdir data\ssc\বাংলা_১ম_পত্র\নৌকাডুবি
+echo ✅ সব ফোল্ডার তৈরি হয়েছে!
+pause
